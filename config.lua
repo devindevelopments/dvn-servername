09e68670d9fd4eaf04ec10eac6e71df810d1fa3d
Devin Developments <164824387+devindevelopments@users.noreply.github.com>
2024-06-22 20:09:54 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -13,3 +13,11 @@ Config.PlayerCount = '64'
 Config.Color =  {r = 255, g = 255, b = 255}
 
 Config.Color2 = {r = 248, g = 202, b = 78}
+
+Config.Offset = { x = 0.4, y = 0.96}
+
+Config.Offset2 = { x = 0.4965, y = 0.96}
+
+Config.Offset3 = { x = 0.551, y = 0.96}
+
+Config.Offset4 = { x = 0.577, y = 0.96}
